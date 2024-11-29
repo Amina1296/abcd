16c371548cf5d3ffcf1baea0c0e95d9788e1832b
amina1296 <aminaa258000@gmail.com>
2024-11-29 09:54:00 +0900

    hicheel

---
+++ 01-js/03_variable.js
@@ -0,0 +1,30 @@
+console.log('js connect')
+var amina;
+console.log(amina);
+
+amina = 20;
+console.log(amina);
+
+var amina2=4;
+console.log (amina2);
+var amina2=5;
+console.log (amina2);
+
+amina2=30;
+console.log (amina2);
+
+let b;
+console.log(b);
+b=7;
+console.log(b)
+let b2= 77;
+console.log(b2)
+ b2=78;
+ console.log(b2);
+ const c=3;
+ console.log(3);
+ 
+ 
+
+
+
---
+++ 01-js/04_operator.js
@@ -0,0 +1,33 @@
+let a=5;
+let b=2;
+console.log(a + b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b);
+console.log(a**b)
+
+console.log('-----------')
+console.log(1==1)
+console.log(1==2)
+console.log(1!=2)
+console.log(1!=1)
+console.log('1'==1)
+console.log('1'!=1)
+console.log('-----------')
+console.log(1===1)
+console.log(1===2)
+console.log(1!==1)
+console.log(1!==2)
+console.log('1'===1)
+console.log('1'!==1)
+console.log('-----------')
+console.log(2>1)
+console.log(2>=2)
+console.log(2<1)
+console.log(2<=2)
+console.log('-----------')
+console.log(1=='1')
+console.log('0'==false)
+console.log(''==0)
+console.log(null== undefined)
